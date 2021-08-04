efe5cb508832c4faa9f33866d9e4a043c27ee5fd
Anders Bo Bojesen <anders@socioskop.dk>
2021-08-05 00:52:40 +0200

    balance check after 1st p-score matching

--- lib/match.R
+++ lib/match.R
@@ -5,6 +5,7 @@ rm(list=ls()[!"echo" %in% ls()])
 source("./lib/load.R")
 library(kableExtra)
 library(magrittr)
+library(optmatch)
 
 # reads 1A analysis data
 d <- readRDS("./data/1A_compiled")
@@ -19,11 +20,10 @@ num <- c("age", "unempl", xs[startsWith(xs, "y_")])
 bin <- c("male", xs[startsWith(xs, "b_")])
 
 # crude comparison table, before matching
-o[["t0"]] <- ttools::ttabulate(d, xs, "treat", num, bin=bin, cal.date = "index", show.na = T, cens=0)
-writexl::write_xlsx(o, "./out/t1.xlsx")
+o[["t1_pre"]] <- ttools::ttabulate(d, xs, "treat", num, bin=bin, cal.date = "index", show.na = T, cens=0)
 
 # also print table to png for easy viewing
-knitr::kable(o[["t0"]],
+knitr::kable(o[["t1_pre"]],
              format = "html",
              align = "lrrlrlr" ,
              escape = F, row.names = F, caption = "Balance between groups before matcing") %>% 
@@ -41,9 +41,42 @@ knitr::kable(o[["t0"]],
 # estimating propensity score:
 # - using simple logit regression to avoid overfitting 
 # - we might switch the model or stratify according to e.g. gender in order to have a reasonable balance after matching
-form <- xs[!xs %in% c("n", "id", "index", "treat")]
-form <- paste0("as.num(treat=='ONE')~", paste0(form, collapse="+"))
+prds <- xs[!xs %in% c("n", "id", "index", "treat", "unempl", "age")] # unempl not a p-score factor
+form <- paste0("as.num(treat=='ONE')~poly(age, 2)+", paste0(prds, collapse="+"))
 m <- glm(form, binomial, d)
-print(list("fit has issues: "=summary(m)))
+print(list("fit is reasonable: "=summary(m)))
 
-# 
+# get predictions / propensity score
+d$p <- predict(m)
+
+# prepare input vectors for optimal matching
+p <- d$p
+names(p) <- rownames(d)
+z <- as.num(d$treat=='ONE')
+names(z) <- d$treat
+
+match <- optmatch::pairmatch(p, z=z, controls=2, data=d)
+d$match <- as.num(rownames(d) %in% names(match[!is.na(match)]))
+d$match <- ifelse(d$match==1 & d$treat=="ONE", "ONE", ifelse(d$match==1 & d$treat=="INT", "ctrl", NA))
+
+# crude comparison table, after matching
+o[["t1_post"]] <- ttools::ttabulate(d, xs, "match", num, bin=bin, cal.date = "index", show.na = T, cens=0)
+writexl::write_xlsx(o, "./out/t1.xlsx")
+
+# works really well for first attempt. Might keep it here, seems reasonable.
+
+# also print table to png for easy viewing
+knitr::kable(o[["t1_post"]],
+             format = "html",
+             align = "lrrlrlr" ,
+             escape = F, row.names = F, caption = "Balance between groups before matcing") %>% 
+  kable_styling(
+    full_width = F,
+    bootstrap_options = c("hover", "condensed" , "bordered"),
+    font_size = 14,
+    position = "left"
+  ) %>% footnote(
+    general = paste0("ctrl is the matched comparison group (2:1), ONE is the 1A treatment group"),
+    footnote_as_chunk = F,
+    escape = T, general_title = ""
+  ) %>% kableExtra::as_image(file = "./out/t1_match.png")
