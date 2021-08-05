860ca80e927dfee941bb911e07edd89d14a9f526
Anders Bo Bojesen <anders@socioskop.dk>
2021-08-05 09:24:00 +0200

    initiating effects evaluation

--- lib/effects.R
+++ lib/effects.R
@@ -14,9 +14,22 @@ d$id <- paste0("i", d$id)
 d <- merge(readRDS("./data/match"), 
            d, by="id")
 
-# configuring time-to-event (tte) outcomes 
-d$bin_full <- as.num(d$tte_full<=104)
-d$bin_full[is.na(d$tte_full)] <- 0
+# configuring 6 months (26 weeks) time-to-event (tte) outcomes 
+d$bin_full06 <- as.numeric(NA)
+d$tte_full06 <- as.numeric(d$tte_full)
+d$bin_full06 <- as.num(d$tte_full06<=26) # upper limit/horizon is 26 weeks (~2)
+d$bin_full06[is.na(d$tte_full06)] <- 0
 
-d$tte_full[d$tte_full>104] <- 104
-s <- surv
+d$tte_full06[d$tte_full06>26] <- 26
+d$tte_full06[d$bin_full06==0] <- 26
+
+# configuring 12 months (52 weeks) time-to-event (tte) outcome 
+d$bin_full12 <- as.numeric(NA)
+d$tte_full12 <- as.numeric(d$tte_full)
+d$bin_full12 <- as.num(d$tte_full12<=52) # upper limit/horizon is 52 weeks (~2)
+d$bin_full12[is.na(d$tte_full12)] <- 0
+
+d$tte_full12[d$tte_full12>52] <- 52
+d$tte_full12[d$bin_full12==0] <- 52
+
+
